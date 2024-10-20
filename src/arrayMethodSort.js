626753ef9e106b74cb84b799e0b9e2c6457d7e9a
Łukasz <1luki9901@gmail.com>
2024-10-20 23:06:28 +0200

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,27 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    if (typeof compareFunction !== 'function') {
+      // eslint-disable-next-line no-param-reassign
+      compareFunction = function (a, b) {
+        if (a === b) {
+          return 0;
+        }
+
+        return a < b ? -1 : 1;
+      };
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      for (let j = 0; j < this.length - 1 - i; j++) {
+        if (compareFunction(this[j], this[j + 1]) > 0) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
